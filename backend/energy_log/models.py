ab3bbc8d42b3d1e3fb1e34bcfdc5badee4d84935
k-aizawa1991 <aizawa@udzuki.co.jp>
2023-05-24 11:20:14 +0900

    Add log model

--- backend/energy_log/models.py
+++ backend/energy_log/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
+class EnergyLog(models.Model):
+    """電力ログ"""
+    date = models.DateField(verbose_name='日付')
+    generation_amount = models.DecimalField(
+        verbose_name='発電量',
+        max_digits=10,
+        decimal_places=2,
+        blank=True,
+        null=True)
+
 # Create your models here.
